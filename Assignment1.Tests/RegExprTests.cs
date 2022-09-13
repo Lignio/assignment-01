08badecb32dc87df51036721bc2bc8bb9c2a6911
adamjhr <68515776+adamjhr@users.noreply.github.com>
2022-09-13 15:12:54 +0200

    Regex (#1)
    * Tests for final regex exercises
    
    * implemented methods for regex exercises

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -34,13 +34,45 @@ public class RegExprTests
     }
 
     [Fact]
-    public void TestInnerText()
+    public void InnerTextOfLinks()
     {
         //Assign
-        string[] test = {""};
-    
+        string html = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href='https://en.wikipedia.org/wiki/Theoretical_computer_science' title='Theoretical computer science'>theoretical computer science</a> and <a href='https://en.wikipedia.org/wiki/Formal_language' title='Formal language'>formal language</a> theory, a sequence of <a href='https://en.wikipedia.org/wiki/Character_(computing)' title='Character (computing)'>characters</a> that define a <i>search <a href='https://en.wikipedia.org/wiki/Pattern_matching' title='Pattern matching'>pattern</a></i>. Usually this pattern is then used by <a href='https://en.wikipedia.org/wiki/String_searching_algorithm' title='String searching algorithm'>string searching algorithms</a> for 'find' or 'find and replace' operations on <a href='https://en.wikipedia.org/wiki/String_(computer_science)' title='String (computer science)'>strings</a>.</p></div>";
+        string tag = "a";
+
         //Act
-    
+        var innerText = RegExpr.InnerText(html, tag);
+
         //Assert
+        innerText.Should().BeEquivalentTo(new [] {"theoretical computer science", "formal language", "characters", "pattern", "string searching algorithms", "strings"});
+        // Assert.Equal(new [] {"theoretical computer science", "formal language", "characters", "pattern", "string searching algorithms", "strings"}, innerText);
+    }
+
+    [Fact]
+    public void ReturnsUrlsWithTitles() {
+
+        // Assign
+        string html = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href='https://en.wikipedia.org/wiki/Theoretical_computer_science' title='Theoretical computer science'>theoretical computer science</a> and <a href='https://en.wikipedia.org/wiki/Formal_language' title='Formal language'>formal language</a> theory, a sequence of <a href='https://en.wikipedia.org/wiki/Character_(computing)' title='Character (computing)'>characters</a> that define a <i>search <a href='https://en.wikipedia.org/wiki/Pattern_matching' title='Pattern matching'>pattern</a></i>. Usually this pattern is then used by <a href='https://en.wikipedia.org/wiki/String_searching_algorithm' title='String searching algorithm'>string searching algorithms</a> for 'find' or 'find and replace' operations on <a href='https://en.wikipedia.org/wiki/String_(computer_science)' title='String (computer science)'>strings</a>.</p></div>";
+
+        // Act
+        var urlOrInnerText = RegExpr.Urls(html);
+
+        // Assert
+        urlOrInnerText.Should().BeEquivalentTo(new [] {(new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"), (new Uri("https://en.wikipedia.org/wiki/Formal_language"), "Formal language"), (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"), "Character (computing)"), (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"), "Pattern matching"), (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)")});
+        // Assert.Equal(new [] {(new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"), (new Uri("https://en.wikipedia.org/wiki/Formal_language"), "Formal language"), (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"), "Character (computing)"), (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"), "Pattern matching"), (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)")}, urlOrInnerText);
+    }
+
+    [Fact]
+    public void ReturnsUrlsWithTitlesLastLinkNoTitle() {
+
+        // Assign
+        string html = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href='https://en.wikipedia.org/wiki/Theoretical_computer_science' abc='jaba' title='Theoretical computer science'>theoretical computer science</a> and <a href='https://en.wikipedia.org/wiki/Formal_language' title='Formal language'>formal language</a> theory, a sequence of <a href='https://en.wikipedia.org/wiki/Character_(computing)' title='Character (computing)'>characters</a> that define a <i>search <a href='https://en.wikipedia.org/wiki/Pattern_matching' title='Pattern matching'>pattern</a></i>. Usually this pattern is then used by <a title='String searching algorithm' href='https://en.wikipedia.org/wiki/String_searching_algorithm'>string searching algorithms</a> for 'find' or 'find and replace' operations on <a href='https://en.wikipedia.org/wiki/String_(computer_science)'>strings</a>.</p></div>";
+
+        // Act
+        var urlOrInnerText = RegExpr.Urls(html);
+
+        // Assert
+        // urlOrInnerText.Should().BeEquivalentTo(new [] {(new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"), (new Uri("https://en.wikipedia.org/wiki/Formal_language"), "Formal language"), (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"), "Character (computing)"), (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"), "Pattern matching"), (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "strings")});
+        Assert.Equal(new [] {(new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"), (new Uri("https://en.wikipedia.org/wiki/Formal_language"), "Formal language"), (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"), "Character (computing)"), (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"), "Pattern matching"), (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "strings")}, urlOrInnerText);
     }
 }
